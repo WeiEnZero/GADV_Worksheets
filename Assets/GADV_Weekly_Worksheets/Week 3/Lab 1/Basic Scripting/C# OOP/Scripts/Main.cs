1f46bb4160f9d5c998b921db375e2763d84e7703
WeiEnZero <shutman89@gmail.com>
2025-05-22 17:20:58 +0800

    New Script

--- Assets/GADV_Weekly_Worksheets/Week 3/Lab 1/Basic Scripting/C# OOP/Scripts/Main.cs
+++ Assets/GADV_Weekly_Worksheets/Week 3/Lab 1/Basic Scripting/C# OOP/Scripts/Main.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Runtime.CompilerServices;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class Main : MonoBehaviour
@@ -8,9 +10,33 @@ public class Main : MonoBehaviour
         //Player player = new Player(10);
         //player.TakeDamage(3);
 
+        Health health = new Health();
+        health.GetHealth();
+        health.TakeDamage(100);
+        health.GetHealth();
         Projectile projectile = new Projectile(1f);
         projectile.Fire();
         
     }
+
+}
+
+public class Health
+{
+    private int health = 100;
+
+    public void TakeDamage(int damage)
+    {
+        if (health > 0)
+        {
+            health-=100;
+        }
+    }      
+
+    public void GetHealth()
+    {
+        Debug.Log("Your health is " + health);
+    }
+
 
 }
